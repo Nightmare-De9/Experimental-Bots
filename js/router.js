47e4beae2d4634a57aff5f61307f9a7830be16bf
Mr.Nightmare_HDH <138434052+Nightmare-De9@users.noreply.github.com>
2026-01-15 18:42:31 +0530

    Delete js directory

--- js/router.js
+++
@@ -1,5 +0,0 @@
-const params = new URLSearchParams(location.search);
-const botKey = params.get("bot") || "beta";
-const BOT = Bots[botKey];
-
-document.getElementById("botTitle").innerText = BOT.name;
